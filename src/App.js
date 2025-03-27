d3a5ff9be969ba3fae672e8ab0bfcb62ced64807
SMG <SMG05eoin@gmail.com>
2025-03-28 00:39:36 +0900

    react설치치

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class>
+      리엑트<br></br>
+      오 개신기해!!ss
+    </div>
+  );
+}
+
+export default App;
