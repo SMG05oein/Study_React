658f8f7a7a4b6f409437e4e18014af7cef1ab47d
SMG <SMG05eoin@gmail.com>
2025-03-28 00:46:32 +0900

    코드 재활용 전

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div class>
-      리엑트<br></br>
-      오 개신기해!!ss
+    <div>
+      <div className="box">
+      리엑트1<br></br>
+      오 개신기해!!<br></br>
+      </div>
+
+      <div className="box">
+      리엑트2<br></br>
+      오 개신기해!!<br></br>
+      </div>
+
+      <div className="box">
+      리엑트3<br></br>
+      오 개신기해!!<br></br>
+      </div>
     </div>
   );
 }
