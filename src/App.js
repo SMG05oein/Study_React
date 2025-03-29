b59c50f46f9e0d0c6a52e0ccecbe6fe52281c625
SMG <SMG05eoin@gmail.com>
2025-03-30 00:10:17 +0900

    lifeCycle 끝

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Timer from './timer';
 
 function App() {
   const [count, setCount] = useState(0);
   const [value, setValue] = useState(0);
-
+  const [showTime, setShowTimer] = useState(true);
   const handleClick = () => {
     setCount((prev) => prev + 1);
     setValue((prev) => prev + 1);
-    // setValue(value+1); //set함수는 비동기이기에 여러면 호출되면 개발자 오류 발생
+    // setValue(value+1); //set함수는 비동기이기에 여러번 호출되면 의도와 다른 결과를 출력 할 수도 있음
   };
 
   useEffect(() => {
@@ -18,7 +19,13 @@ function App() {
 
   useEffect(() => {
     console.log("업데이트");
-  }, [count]); //배열 안에 있는 state가 업데이트되면 콜백 함수도 호출 됨
+  }, [count, value]); //배열 안에 있는 state가 업데이트되면 콜백 함수도 호출 됨
+
+  useEffect(() => {
+    console.log("다른 일");
+  }, [value]);
+
+  
 
   return (
     <div className="App">
@@ -26,6 +33,8 @@ function App() {
       <h1>{count}</h1>
       <h1>{value}</h1>
       <button onClick={handleClick}>증가</button>
+      <button onClick={() => setShowTimer(prev => !prev)}>타이머 보이기</button>
+      {showTime && <Timer />}
     </div>
   );
 }
